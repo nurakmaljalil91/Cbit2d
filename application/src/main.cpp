7ad68a2a3c032b0b4503e712cbfd334dc3051023
Nur Akmal Jalil <48422687+nurakmaljalil91@users.noreply.github.com>
2024-07-22 01:24:32 +0800

    test SDL Image

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -12,19 +12,104 @@
  * @date 2024-07-20
  */
 
-#include "../../src/core/Application.h"
-#include "scenes/CustomScene.h"
+//
 
-int main(int argc, char* argv[]) {
-    Application app("Cbit 2D Game Engine", 800, 600);
-    if (!app.init()) {
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int SPRITE_WIDTH = 32;
+const int SPRITE_HEIGHT = 32;
+
+SDL_Texture* loadTexture(std::string path, SDL_Renderer* renderer) {
+    SDL_Texture* newTexture = nullptr;
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if (!loadedSurface) {
+        std::cerr << "Unable to load image " << path << "! SDL_image Error: " << IMG_GetError() << std::endl;
+    } else {
+        newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+        if (!newTexture) {
+            std::cerr << "Unable to create texture from " << path << "! SDL Error: " << SDL_GetError() << std::endl;
+        }
+        SDL_FreeSurface(loadedSurface);
+    }
+    return newTexture;
+}
+
+void renderTexture(SDL_Texture* texture, SDL_Renderer* renderer, SDL_Rect* clip, int x, int y) {
+    SDL_Rect renderQuad = { x, y, clip->w, clip->h };
+    SDL_RenderCopy(renderer, texture, clip, &renderQuad);
+}
+
+int main(int argc, char* args[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    app.getSceneManager().addScene("CustomScene",std::make_shared<CustomScene>());
-    app.getSceneManager().setCurrentScene("CustomScene");
+    SDL_Window* window = SDL_CreateWindow("SDL2 Sprite Sheet", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-    app.run();
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        std::cerr << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture* spriteSheetTexture = loadTexture("resources/test.png", renderer);
+    if (!spriteSheetTexture) {
+        std::cerr << "Failed to load sprite sheet texture!" << std::endl;
+        return 1;
+    }
+
+    SDL_Rect spriteClips[4];
+    for (int i = 0; i < 4; ++i) {
+        spriteClips[i].x = i * SPRITE_WIDTH;
+        spriteClips[i].y = 0;
+        spriteClips[i].w = SPRITE_WIDTH;
+        spriteClips[i].h = SPRITE_HEIGHT;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+
+        renderTexture(spriteSheetTexture, renderer, &spriteClips[0], 50, 50);
+        renderTexture(spriteSheetTexture, renderer, &spriteClips[1], 100, 50);
+        renderTexture(spriteSheetTexture, renderer, &spriteClips[2], 150, 50);
+        renderTexture(spriteSheetTexture, renderer, &spriteClips[3], 200, 50);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(spriteSheetTexture);
+    IMG_Quit();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
